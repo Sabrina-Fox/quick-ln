98915d03649097772407999042523d1a4f646fd4
SabrinaRDC <74781086+Sabrina-Fox@users.noreply.github.com>
2024-09-21 22:56:54 +0800

    converted input fields to table

--- src/index.js
+++ src/index.js
@@ -34,23 +34,23 @@ async function getList() {
         return;
     };
     let HTML = `<tr>
-                    <th>Path</th>
-                    <th>Destination</th>
-                    <th>Use</th>
-                    <th>Limit</th>
-                    <th>Creation</th>
-                    <th>Last Used</th>
-                    <th></th>
+                    <th class="list">Path</th>
+                    <th class="list">Destination</th>
+                    <th class="list">Use</th>
+                    <th class="list">Limit</th>
+                    <th class="list">Creation</th>
+                    <th class="list">Last Used</th>
+                    <th class="list"></th>
                 </tr>`;
     resObject.links.forEach(element => {
         HTML+= `<tr>
-                    <td><a href="${`${resObject.url}/${resObject.prefix}/${element.path}`}" target="_blank">${element.path}</a></td>
-                    <td><a href="${element.destination}" target="_blank">${element.destination}</a></td>
-                    <td>${element.use_count}</td>
-                    <td>${element.use_limit}</td>
-                    <td>${element.creation_time}</td>
-                    <td>${element.last_used}</td>
-                    <td><button onclick="deleteLink('${element.id}')" class="delete">Delete</button></td>
+                    <td class="list"><a href="${`${resObject.url}/${resObject.prefix}/${element.path}`}" target="_blank">${element.path}</a></td>
+                    <td class="list"><a href="${element.destination}" target="_blank">${element.destination}</a></td>
+                    <td class="list">${element.use_count}</td>
+                    <td class="list">${element.use_limit}</td>
+                    <td class="list">${element.creation_time}</td>
+                    <td class="list">${element.last_used}</td>
+                    <td class="list"><button onclick="deleteLink('${element.id}')" class="delete">Delete</button></td>
                 </tr>`;
     });
     lnList.innerHTML = HTML;
