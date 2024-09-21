d7eacb58dda20d8a5bfe787c6bb41f61eb9d07ea
SabrinaRDC <74781086+Sabrina-Fox@users.noreply.github.com>
2024-09-21 22:16:47 +0800

    changed frontend list display to table

--- src/index.js
+++ src/index.js
@@ -33,18 +33,26 @@ async function getList() {
         getErrorText.innerText = resObject.auth_message;
         return;
     };
-    let HTML = '';
+    let HTML = `<tr>
+                    <th>Path</th>
+                    <th>Destination</th>
+                    <th>Use</th>
+                    <th>Limit</th>
+                    <th>Creation</th>
+                    <th>Last Used</th>
+                    <th></th>
+                </tr>`;
     resObject.links.forEach(element => {
-        HTML+= `<div>
-                <p>Path: </p><a href="${`${resObject.url}/${resObject.prefix}/${element.path}`}" target="_blank">${element.path}</a>
-                <p>Destination: </p><a href="${element.destination}" target="_blank">${element.destination}</a>
-                <p>Use: ${element.use_count}</p>
-                <p>Limit: ${element.use_limit}</p>
-                <p>Creation: ${element.creation_time}</p>
-                <p>Last Used: ${element.last_used}</p>
-                <button onclick="deleteLink('${element.id}')" class="delete">Delete</button>
-                <p id="${element.id}" class="delete_error"></p>
-                <br></div>`;
+        HTML+= `<tr>
+                    <td><a href="${`${resObject.url}/${resObject.prefix}/${element.path}`}" target="_blank">${element.path}</a></td>
+                    <td>https://tenor.com/view/sniff-gif-26460635 </td>
+                    <td><a href="${element.destination}" target="_blank">${element.destination}</a></td>
+                    <td>${element.use_count}</td>
+                    <td>${element.creation_time}</td>
+                    <td>${element.last_used}</td>
+                    <td><button onclick="deleteLink('${element.id}')" class="delete">Delete</button></td>
+                    <td><p id="${element.id}" class="delete_error"></p></td>
+                </tr>`;
     });
     lnList.innerHTML = HTML;
 };
